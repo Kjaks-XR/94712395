e3174284b57cb788c3e78b756e7dd4d7c45b46d6
Kjaks-XR <gabrielxware@gmail.com>
2025-08-07 22:50:46 +0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -374,14 +374,14 @@ local Library = {
     RegistryMap = {};
 
     HudRegistry = {};
-
+    InactiveColor = Color3.fromRGB(60, 60, 60),
+    InactiveColorDark =  Color3.fromRGB(60, 60, 60),
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
     AccentColor = Color3.fromRGB(0, 85, 255);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
-
     Black = Color3.new(0, 0, 0);
     Font = Enum.Font.Code,
 
@@ -392,8 +392,6 @@ local Library = {
     ScreenGui = ScreenGui;
 };
 
-Library.InactiveColor = Color3.fromRGB(60, 60, 60);
-Library.InactiveColorDark = Library:GetDarkerColor(Library.InactiveColor);
 
 local RainbowStep = 0
 local Hue = 0
